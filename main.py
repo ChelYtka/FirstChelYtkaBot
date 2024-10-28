2bedf9394a5c90a220494f6a765371475cb4c42d
Egor Danilkin <edanilkin666@mail.ru>
2024-10-29 00:00:10 +0700

    first

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import requests
+import time
 
-api_url = 'http://api.open-notify.org/iss-now.json'
+API_URL = 'https://api.telegram.org/bot' # адрес обращения
+BOT_TOKEN = '7434137903:AAFuMOeu5bLffiXR35lpLLAaHaCQfB0ACds' # токен бота
+TEXT = 'Hello world!!!' # текстовый ответ
+MAX_COUNTER = 10
 
-response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
+offset = -2 # -1 - возвращает последний апдейт, 0 - ничего не возвращает
+counter = 0
+chat_id: int # id чата
 
-if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    print(response.text)
-else:
-    print(response.status_code)  # При другом коде ответа выводим этот код
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+    # делаем get-запрос и т.к. формат ответа известен, распарсим JSON с помощью метода json()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    # json() создаёт многоуровневый словарь
+
+    if updates['result']: # проверяем, есть ли обновление
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
