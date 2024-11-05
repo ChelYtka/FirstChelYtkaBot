bb3dc55b0ba182d33f80cbae6b305eb909c8fb1b
Egor Danilkin <edanilkin666@mail.ru>
2024-11-06 00:29:56 +0700

    добавленый картинки

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import time
 API_URL = 'https://api.telegram.org/bot' # адрес обращения
 f = open('token.txt')
 BOT_TOKEN = f.readline()# токен бота
-TEXT = 'Hello world!!!' # текстовый ответ
+TEXT = 'Hello my dear CAT!!!' # текстовый ответ
+ERROR_TEXT = 'Здесь должен быть кот('
 MAX_COUNTER = 10
 
 offset = -2 # -1 - возвращает последний апдейт, 0 - ничего не возвращает
 counter = 0
 chat_id: int # id чата
+
+API_CAT_URL = 'https://api.thecatapi.com/v1/images/search'
 
 while counter < MAX_COUNTER:
 
@@ -19,10 +22,21 @@ while counter < MAX_COUNTER:
     # json() создаёт многоуровневый словарь
 
     if updates['result']: # проверяем, есть ли обновление
+
         for result in updates['result']:
+
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+            cat_response = requests.get(API_CAT_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
 
     time.sleep(1)
     counter += 1
